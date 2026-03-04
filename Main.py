f26c007963eab7bfd9d9e93283e7ca392eb1a213
Nino-7w <nino.saillard39@gmail.com>
2026-03-04 13:59:25 +0100

    Delete Main.py

--- Main.py
+++
@@ -1,9 +0,0 @@
-#######################################################################
-#                     Projet : Lancé de dé                            #
-#######################################################################
-
-# Ce fichier lance et organise le programme afin d'excécuter toutes les instructions dans le bon ordre.
-# Ce fichier sera donc celui à éxécuter afin de jouer au jeu
-
-def jeu_principale():
-    return lancer_de_dés() 
