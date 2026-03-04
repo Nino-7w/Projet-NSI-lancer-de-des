e3d0c3db20751bcee25c766540bb4f6cdf42b9a5
Nino-7w <nino.saillard39@gmail.com>
2026-03-04 13:59:12 +0100

    Delete Interface graphique.py

--- Interface graphique.py
+++
@@ -1,7 +0,0 @@
-#######################################################################
-#                     Projet : Lancé de dé                            #
-#######################################################################
-
-# Ce fichier permet de gérer l'interface graphique du programme.
-# L'interface facilite l'utilisation du programme.
-
